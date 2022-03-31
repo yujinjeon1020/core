d2ca31f578e53318c70a48b5c3a27723ef98adb5
yujinjeon1020 <yujinjeon1020@gmail.com>
2022-03-31 12:40:40 +0900

    테스트 코드 추가

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -22,4 +22,18 @@ class RateDiscountPolicyTest {
         //then
         Assertions.assertThat(discount).isEqualTo(1000);
     }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        //Assertions.assertThat(discount).isEqualTo(1000);    //실패
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
 }
